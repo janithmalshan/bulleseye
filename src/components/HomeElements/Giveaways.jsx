f594456061bcb45378288ce49d09defef009a588
Janith Malshan <janithmalshan@gmail.com>
2020-11-30 02:11:37 +0200

    Givaway section

--- src/components/HomeElements/Giveaways.jsx
+++ src/components/HomeElements/Giveaways.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import icGuarantee from "../../assets/images/ic-guarantee.svg";
+import imgGiveaway from "../../assets/images/img-giveaway.svg";
 
 export const Giveaways = () => {
     return (
-        <h1 className="text-center">Giveaways</h1>
+        <section className="ui-main__sec-giveaway">
+            <div className="ui-giveaway">
+                <h1>WEEKLY GIVEAWAYS</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non tortor eu est sollicitudin mattis id
+                    quis diam. Suspendisse sagittis condimentum ligula a ullamcorper. Vestibulum ante ipsum primis in
+                    faucibus orci luctus et ultrices posuere cubilia curae.</p>
+                <h2>$25, $50, $100, $250, $500</h2>
+                <img src={imgGiveaway} className="img"/>
+                <button className="ui-btn">SUBSCRIBE NOW!</button>
+            </div>
+        </section>
     )
 };
--- src/components/HomeElements/Subscription.jsx
+++ src/components/HomeElements/Subscription.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import logo from "../../assets/images/ic-petcharity.svg"
 
 export const Subscription = () => {
     return (
         <section className="ui-main__sec-subscription">
-            <h2>Why Choose Us Than Others</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div className="ui-subscription">
+                <h1>25¢ per subscription goes to</h1>
+                <img src={logo} className="img"/>
+                <a target="_blank" href={'https://petsmartcharities.org/'}>https://petsmartcharities.org/</a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non tortor eu est sollicitudin mattis id
+                    quis diam. Suspendisse sagittis condimentum ligula a ullamcorper. Vestibulum ante ipsum primis in
+                    faucibus orci luctus et ultrices posuere cubilia curae.</p>
+                <p>In vitae sodales nulla. Nullam viverra ante odio. Etiam felis erat, mattis nec malesuada eget,
+                    sagittis quis libero. Donec quis nulla nec odio fermentum porta. Nunc nisl ligula, elementum ac
+                    ultricies sed, mollis eu ante. Donec posuere imperdiet metus at molestie. Aenean metus urna,
+                    pellentesque varius purus eu, vestibulum sodales lorem.</p>
+            </div>
         </section>
     )
 };
