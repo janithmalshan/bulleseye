bf2df3852f98e8b524615f3c8a6f6eb4a7afb252
Janith Malshan <janithmalshan@gmail.com>
2020-12-02 15:58:01 +0200

    Responsive fixes and slider

--- src/components/HomeElements/SlideShow.jsx
+++ src/components/HomeElements/SlideShow.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import img from "../../assets/images/img-meet.svg"
+import {Slider} from "./Slider";
 
 export const SlideShow = (props) => {
     return (
         <section className="ui-main__sec-slide" id={props.id}>
-            <div className="ui-"></div>
+            <div className="ui-slide">
+                <div className="ui-slide__left">
+                    <div>
+                        <img className="img" src={img}/>
+                        <div>
+                            <h2>MEET BULLSEYE</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non tortor eu est sollicitudin
+                                mattis id quis diam. Suspendisse sagittis condimentum ligula a ullamcorper. Vestibulum ante
+                                ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="ui-slide__right">
+                    <Slider/>
+                </div>
+            </div>
         </section>
     )
 };
---
+++ src/components/HomeElements/Slider.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import img1 from "../../assets/images/img-s3.png"
+import img2 from "../../assets/images/img-s2.png"
+import img3 from "../../assets/images/img-s1.png"
+
+export class Slider extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            images: [
+                img1,
+                img2,
+                img3,
+                "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/canyon.jpg",
+                "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/mountains.jpg",
+                "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/redsky.jpg"
+            ],
+            currentIndex: 0,
+            translateValue: 0
+        }
+    }
+
+    goToPrevSlide = () => {
+        if(this.state.currentIndex === 0)
+            return;
+
+        this.setState(prevState => ({
+            currentIndex: prevState.currentIndex - 1,
+            translateValue: prevState.translateValue + this.slideWidth()
+        }))
+    }
+
+    goToNextSlide = () => {
+        // Exiting the method early if we are at the end of the images array.
+        // We also want to reset currentIndex and translateValue, so we return
+        // to the first image in the array.
+        if(this.state.currentIndex === this.state.images.length - 1) {
+            return this.setState({
+                currentIndex: 0,
+                translateValue: 0
+            })
+        }
+
+        // This will not run if we met the if condition above
+        this.setState(prevState => ({
+            currentIndex: prevState.currentIndex + 1,
+            translateValue: prevState.translateValue + -(this.slideWidth())
+        }));
+    }
+
+    slideWidth = () => {
+        return document.querySelector('.slide').clientWidth
+    }
+
+    render() {
+        return (
+            <div className="slider">
+
+                <div className="slider-wrapper"
+                     style={{
+                         transform: `translateX(${this.state.translateValue}px)`,
+                         transition: 'transform ease-out 0.45s'
+                     }}>
+                    {
+                        this.state.images.map((image, i) => (
+                            <Slide key={i} image={image} />
+                        ))
+                    }
+                </div>
+
+                <div className="slider-overlay"></div>
+
+                <LeftArrow
+                    goToPrevSlide={this.goToPrevSlide}
+                />
+
+                <RightArrow
+                    goToNextSlide={this.goToNextSlide}
+                />
+            </div>
+        );
+    }
+}
+
+
+const Slide = ({ image }) => {
+    const styles = {
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: '50% 60%'
+    }
+    return <div className="slide" style={styles}></div>
+}
+
+
+const LeftArrow = (props) => {
+    return (
+        <div className="backArrow arrow" onClick={props.goToPrevSlide}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="24" viewBox="0 0 15 24">
+                <path id="ic_chevron_right_24px" d="M11.444,6,8.59,8.82,17.861,18,8.59,27.18,11.444,30,23.59,18Z" transform="translate(-8.59 -6)" fill="#fff"/>
+            </svg>
+        </div>
+    );
+}
+
+
+const RightArrow = (props) => {
+    return (
+        <div className="nextArrow arrow" onClick={props.goToNextSlide}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="24" viewBox="0 0 15 24">
+                <path id="ic_chevron_right_24px" d="M11.444,6,8.59,8.82,17.861,18,8.59,27.18,11.444,30,23.59,18Z" transform="translate(-8.59 -6)" fill="#fff"/>
+            </svg>
+        </div>
+    );
+}
