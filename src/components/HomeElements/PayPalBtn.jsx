779056fcade8c371f3af9c16f6faa1e2be005a55
Janith Malshan <janithmalshan@gmail.com>
2020-12-05 18:49:03 +0200

    test 2

--- src/components/HomeElements/PayPalBtn.jsx
+++ src/components/HomeElements/PayPalBtn.jsx
@@ -1,3 +1,52 @@
+// import React, { useRef, useEffect } from "react";
+//
+// export default function PaypalBtn() {
+//     const paypal = useRef();
+//     useEffect(() => {
+//         window.paypal
+//             .Buttons({
+//                 createSubscription: function(data, actions) {
+//                     return actions.subscription.create({
+//                         'plan_id': 'P-4L440189JC489602DL7EH2QY' // Creates the subscription
+//                     });
+//                 },
+//                 // onApprove: function(data, actions) {
+//                 //     alert('You have successfully created subscription ' + data.subscriptionID); // Optional message given to subscriber
+//                 // },
+//                 createOrder: (data, actions, err) => {
+//                     return actions.order.create({
+//                         intent: "CAPTURE",
+//                         purchase_units: [
+//                             {
+//                                 description: "Cool looking table",
+//                                 amount: {
+//                                     currency_code: "USD",
+//                                     value: 0.01,
+//                                 },
+//                             },
+//                         ],
+//                     });
+//                 },
+//                 onApprove: async (data, actions) => {
+//                     const order = await actions.order.capture();
+//                     console.log(order);
+//                 },
+//                 onError: (err) => {
+//                     console.log(err);
+//                 },
+//             })
+//             .render(paypal.current);
+//     }, []);
+//
+//     return (
+//         <div>
+//             <div ref={paypal}></div>
+//         </div>
+//     );
+// }
+
+
+
 import { PayPalButton } from "react-paypal-button-v2";
 import React from 'react';
 export function PayPalBtn(props) {
--- src/components/HomeElements/Subscribe2.jsx
+++ src/components/HomeElements/Subscribe2.jsx
@@ -1,50 +1,47 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import payTypes from "../../assets/images/ic-subscribe-iconset.svg"
 import labelPercent50 from "../../assets/images/ic-deal-50.svg"
 import labelPercent65 from "../../assets/images/ic-deal-65.svg"
 import imgSubscribe from "../../assets/images/img-subscribe.svg"
-import PayPalButton from "./PayPalBtn"
+// import PayPalButton from "./PayPalBtn"
+import ReactDOM from "react-dom"
 
-const paypalSubscribe = (data, actions) => {
-    return actions.subscription.create({
-        'plan_id': "P-4L440189JC489602DL7EH2QY",
-    });
-};
-const paypalOnError = (err) => {
-    console.log("Error")
-    console.log(err)
-};
-const paypalOnApprove = (data, detail) => {
-// call the backend api to store transaction details
-    console.log("Payapl approved")
-    console.log(data.subscriptionID)
-};
+import PayPal from "./PayPalBtn";
+
+// const paypalSubscribe = (data, actions) => {
+//     return actions.subscription.create({
+//         'plan_id': "P-9BV25233YH3816747L7EHYLY",
+//     });
+// };
+// const paypalOnError = (err) => {
+//     console.log("Error")
+// };
+// const paypalOnApprove = (data, detail) => {
+// // call the backend api to store transaction details
+//     console.log("Payapl approved")
+//     console.log(data.subscriptionID)
+// };
 
 export const Subscribe = (props) => {
     const paypal = useRef();
+    const PayPalButton = paypal.Buttons.driver("react", { React, ReactDOM });
+    const [checkout, setCheckout] = React.useState(false);
+    const createOrder = (data, actions) =>{
+        return actions.order.create({
+            purchase_units: [
+                {
+                    amount: {
+                        value: "0.01",
+                    },
+                },
+            ],
+        });
+    };
 
+    const onApprove = (data, actions) => {
+        return actions.order.capture();
+    };
 
-    useEffect(() => {
-        window.paypal
-            .Buttons({
-                createSubscription: function(data, actions) {
-                    return actions.subscription.create({
-                        'plan_id': 'P-4L440189JC489602DL7EH2QY' // Creates the subscription
-                    });
-                },
-                // onApprove: function(data, actions) {
-                //     alert('You have successfully created subscription ' + data.subscriptionID);
-                // },
-                onApprove: async (data, actions) => {
-                    const order = await actions.order.capture();
-                    console.log(order);
-                },
-                onError: (err) => {
-                    console.log(err);
-                },
-            }).render('#paypal-button-container');
-            // .render(paypal.current);
-    }, []);
     return (
         <section className="ui-main__sec-subscribe" id={props.id}>
             <div className="ui-subscribe">
@@ -69,20 +66,25 @@ export const Subscribe = (props) => {
                         <h1>$ 2.95/month</h1>
                         <h2>$ 5.90/month</h2>
                         <p>Less than 10¢ a day</p>
-                        <div id="paypal-button-container"></div>
                         <p>25¢ per subscription goes to the PetSmart charities</p>
                         <p>Weekly Gift Card Giveaways!</p>
                         <img src={payTypes} height={30}/>
-                        <PayPalButton
-                            amount = "0.1"
-                            currency = "USD"
-                            createSubscription={paypalSubscribe}
-                            onApprove={paypalOnApprove}
-                            catchError={paypalOnError}
-                            onError={paypalOnError}
-                            onCancel={paypalOnError}
-                        />
+                        {/*<PayPalButton*/}
+                        {/*    amount = "0.1"*/}
+                        {/*    currency = "USD"*/}
+                        {/*    createSubscription={paypalSubscribe}*/}
+                        {/*    onApprove={paypalOnApprove}*/}
+                        {/*    catchError={paypalOnError}*/}
+                        {/*    onError={paypalOnError}*/}
+                        {/*    onCancel={paypalOnError}*/}
+                        {/*/>*/}
                         <img className="ui-label-icon" src={labelPercent50}/>
+                        <PayPalButton
+                            createOrder={(data, actions) => createOrder(data, actions)}
+                            onApprove={(data, actions) => onApprove(data, actions)}
+                        />
+
+                        <PayPal />
                     </div>
                     <div className="ui-subscribe__wr-item">
                         <h1>$ 24.78/year</h1>
